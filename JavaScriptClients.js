c3e02089ccd5d0c7903e6e63399a50de0a971abd
SusanaCV <susanacorrales03@gmail.com>
2015-08-14 20:22:49 -0600

    Js
    Logica de los clientes

--- JavaScriptClients.js
+++ JavaScriptClients.js
@@ -6,12 +6,10 @@ var CLIENT= CLIENT || {
 	  this.telefono = telefono;
 
 	  this.save = function() {
-	  	//insert into 
 	  localStorage.setItem("nombre",this.nombre); 
       localStorage.setItem("cedula",this.cedula); 
       localStorage.setItem("telefono",this.telefono); 
       alert("Datos Guardados en LocalStorage");
-	  	// save to localstorage
 	  };
 	},
 
@@ -19,17 +17,16 @@ var CLIENT= CLIENT || {
 	document.forms[0].nombre.value=localStorage.getItem("nombre");
    document.forms[0].cedula.value=localStorage.getItem("cedula");
    document.forms[0].telefono.value=localStorage.getItem("telefono");
-	alert("Nombre: "+localStorage.getItem("nombre")+"\n"+"Apellido: "+localStorage.getItem("cedula")+"\n"+"Telefono: "+localStorage.getItem("telefono"));
 	},
 
 	saveClient: function() {
-		/*var contents = $('#contents');*/
 		var nombre = document.getElementById('nombre').value;
 		var cedula = document.getElementById('cedula').value;
 		var telefono = document.getElementById('telefono').value;
+		var clientes = [];
+		var cliente = ['carmen','23','888'];
 
-
-		var usuario = new TAREA.Usuario(nombre, cedula,telefono);
+		var usuario = new CLIENT.Usuario(nombre, cedula,telefono);
 		usuario.save();
 	},
 
